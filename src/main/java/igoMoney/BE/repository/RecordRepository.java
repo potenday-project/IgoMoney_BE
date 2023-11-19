80598d49f665bc158194754fabe78701844c64b4
BK <kbomin328@gmail.com>
2023-11-20 01:10:58 +0900

    Fix: 챌린지 totalcost 산출방식 오류 수정

--- src/main/java/igoMoney/BE/repository/RecordRepository.java
+++ src/main/java/igoMoney/BE/repository/RecordRepository.java
@@ -11,8 +11,11 @@ import java.util.List;
 public interface RecordRepository extends JpaRepository<Record, Long> {
     List<Record> findAllByUserIdAndDate(Long userId, LocalDate date);
 
+    @Query(value="SELECT r.user_id, SUM(r.cost) FROM record r WHERE r.challenge_id= :challengeId AND r.user_id= :userId", nativeQuery = true)
+    List<Object[]> calculateTotalCostByUserId(@Param("challengeId") Long challengeId, @Param("userId")Long userId);
+
     @Query(value="SELECT r.user_id, SUM(r.cost) FROM record r WHERE r.challenge_id= :challengeId GROUP BY r.user_id", nativeQuery = true)
-    List<Object[]> calculateTotalCostByUserId(@Param("challengeId") Long challengeId);
+    List<Object[]> calculateTotalCostByChallengeId(@Param("challengeId") Long challengeId);
 
     List<Record> findAllByUserId(Long userId);
     @Query(value="SELECT COUNT(*) FROM record r WHERE r.user_id= :userId AND r.date >= DATE_SUB(:date,INTERVAL 3 DAY)", nativeQuery = true)
